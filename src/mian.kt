63807db472c2682430a415a2c08cee8c076c0d64
Femboy <skibidi@furaffinity.org>
2025-03-26 19:33:20 +0000

    add GPU information retrieval and update project configuration

--- src/mian.kt
+++ src/mian.kt
@@ -1,19 +1,46 @@
+import oshi.SystemInfo
+import oshi.hardware.GraphicsCard
+import oshi.hardware.HardwareAbstractionLayer
+object gpui {
+	var vram = ""
+	var model = ""
+	var RedBlueGreen = ""
+}
 class idk {
 	var distro = ""
 	var kernel = ""
 	var cpu = ""
 	var gpu = ""
-	var ram_usage = ""
+	val ram_usage = 0
 }
 val sys = idk()
 
+fun GetGpu() {
+	val si = SystemInfo()
+	val hardware: HardwareAbstractionLayer = si.hardware
+	val graphicsCards: List<GraphicsCard> = hardware.graphicsCards
+
+	if (!graphicsCards.isEmpty()) {
+		graphicsCards.forEachIndexed { index, gpu ->
+			val vramBytes = gpu.vRam
+			val vramGib = vramBytes / (1024.0 * 1024.0 * 1024.0)
+			gpui.vram = "Vram: ${String.format("%.2f", vramGib)} GiB"
+			gpui.model = "gpu: ${gpu.name}"
+			gpui.RedBlueGreen = "manufacturer: ${gpu.vendor}"
+		}
+	}
+	else {
+		println("No graphics cards found.")
+	}
+}
+
+
 fun main() {
+	GetGpu()
 	sys.distro = System.getProperty("os.name")
 	sys.kernel = System.getProperty("os.version")
-	sys.cpu = System.getProperty("os.arch")
 	println(sys.distro)
 	println(sys.kernel)
-	println(sys.cpu)
-	println(sys.gpu)
-	println(sys.ram_usage)
+	println("━━━━━━━━━━━━━━━━━━━━━━━━")
+	println(gpui.model + "\n" + gpui.RedBlueGreen + "\n" + gpui.vram)
 }
